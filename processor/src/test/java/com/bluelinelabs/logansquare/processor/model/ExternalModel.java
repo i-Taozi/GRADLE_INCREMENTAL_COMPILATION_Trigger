fbf9b3915917c6816004395c253aa0a65d42af9a
i-Taozi <879714748@qq.com>
2022-02-21 14:09:37 +0800

    liato/android-bankdroid:original code

--- processor/src/test/java/com/bluelinelabs/logansquare/processor/model/ExternalModel.java
+++
@@ -1,8 +0,0 @@
-package com.bluelinelabs.logansquare.processor.model;
-
-// Note: intentionally not annotated so we can test loading external JsonObjectMappers
-public class ExternalModel {
-
-    public String string;
-
-}
--- processor/src/test/java/com/bluelinelabs/logansquare/processor/model/TwoParamGenericWithStringModel.java
+++
@@ -1,8 +0,0 @@
-package com.bluelinelabs.logansquare.processor.model;
-
-import com.bluelinelabs.logansquare.annotation.JsonObject;
-
-@JsonObject
-public class TwoParamGenericWithStringModel<K> extends TwoParamGenericModel<String, K> {
-
-}
